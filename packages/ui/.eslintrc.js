be9b6aafb4720104a19d0f66a13aff76c3112363
Danielle Huisman <danielle@nixy.software>
2024-01-20 13:11:42 +0100

    Initial commit

---
+++ packages/ui/.eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: ["@repo/eslint-config/react-internal.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.join(__dirname, "tsconfig.eslint.json"),
+  },
+};
